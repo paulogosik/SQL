a37c904d6968a19c4756ee69539d3b7d2611cb56
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-03-22 11:10:13 -0300

    Create TiposDeModelagem.sql

---
+++ Modelagem e Arquitetura de Software/TiposDeModelagem.sql
@@ -0,0 +1,23 @@
+- Tipo usado no exemplo: Sistema para armazenamento de usuários
+
+/* Modelagem básica
+ - Entidade = Tabela
+ - Campos = Atributos*/
+
+CLIENTE
+
+NOME - CARACTER(30)
+CPF - NUMERICO(11)
+EMAIL - CARACTER(30)
+TELEFONE - CARACTER(30)
+ENDERECO - CARACTER(100)
+SEXO - CARACTER(1)
+
+/*Processos de Modelagem*/
+
+/* FASE 01 E FASE 02 - AD ADM DE DADOS */
+MODELAGEM CONCEITUAL - Rascunho (Assim como feito acima, na Modelagem básica)
+MODELAGEM lÓGICA
+
+/* FASE 03 - DBA / AD */
+MODELAGEM FÍSICA
