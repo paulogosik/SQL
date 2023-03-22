aaead5e8645a0106d91b9def38efb565712d602b
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-03-22 17:05:40 -0300

    Update ModelagemFisica.sql

--- Modelagem e Arquitetura de Software/ModelagemFisica.sql
+++ Modelagem e Arquitetura de Software/ModelagemFisica.sql
@@ -4,9 +4,9 @@ USE projeto
 
 CREATE TABLE Clientes (
 	Nome  VARCHAR (30),
-    CPF  INT(11),
+    CPF  VARCHAR(11),
     Email VARCHAR (30),
     Telefone VARCHAR (30),
     Endereco VARCHAR (100),
     Sexo CHAR (1)
-);
+);	
