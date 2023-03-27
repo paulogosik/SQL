87d5a32882c4ed768e9fb9bf74d3e30dda6840d4
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-03-27 08:35:27 -0300

    Create CriandoTabelaFuncionarios.sql

---
+++ Exercício - Funcionários/CriandoTabelaFuncionarios.sql
@@ -0,0 +1,12 @@
+create table funcionarios
+  (
+      idFuncionario integer,
+      nome varchar(100),
+      email varchar(200),
+      sexo varchar(10),
+      departamento varchar(100),
+      admissao varchar(10),
+      salario integer,
+      cargo varchar(100),
+      idRegiao int
+  );
